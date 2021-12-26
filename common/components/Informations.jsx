584cb2099e461f3d4ac034a42aa72d682de870de
Karleon Cristophe <karleoncristophe@hotmail.com>
2021-12-26 14:35:45 -0300

    new version

--- common/components/Informations.jsx
+++ common/components/Informations.jsx
@@ -105,10 +105,10 @@ const Informations = () => {
       <Subtitle>_Full-Stack Developer_</Subtitle>
       <Text darkMode={darkMode}>
         Atualmente estudando/trabalhando com Frontend e Backend. Amo aprender
-        novas tecnologias e enfrentar novos desafios na programação. Iniciei a
-        carreira visando desenvolvimento de jogos, que é algo que gosto muito,
-        mas com o decorrer do tempo me apaixonei por desenvolvimento Web.
-        Estudo/Trabalho com ReactJS e NodeJS.
+        novas tecnologias e enfrentar novos desafios na programação. Iniciei
+        minha carreira como programador visando desenvolvimento de jogos, que é
+        algo que gosto muito, mas com o decorrer do tempo me apaixonei por
+        desenvolvimento Web. Estudo/Trabalho com ReactJS e NodeJS.
       </Text>
       <Contact />
     </Article>
