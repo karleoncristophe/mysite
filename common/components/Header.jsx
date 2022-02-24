3afeccc0f15efa95b2dd743d415b6a4aa1406397
Karleon Cristophe <karleoncristophe@hotmail.com>
2022-02-24 11:03:07 -0300

    new version

--- common/components/Header.jsx
+++ common/components/Header.jsx
@@ -130,7 +130,7 @@ const socialMedia = [
   {
     id: 0,
     name: 'Facebook',
-    link: 'https://www.facebook.com/karleoncristophe',
+    link: 'https://www.facebook.com/karleoncris',
     image: '/facebook.png',
   },
   {
@@ -142,7 +142,7 @@ const socialMedia = [
   {
     id: 2,
     name: 'Instagram',
-    link: 'https://www.instagram.com/karleoncristophe/',
+    link: 'https://www.instagram.com/karleoncris/',
     image: '/instagram.png',
   },
   {
