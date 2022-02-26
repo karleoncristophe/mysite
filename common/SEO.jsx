f19253365ee46874b25b8738063313f2a8167764
Karleon Cristophe <karleoncristophe@hotmail.com>
2022-02-26 18:10:55 -0300

    link change

--- common/SEO.jsx
+++ common/SEO.jsx
@@ -9,34 +9,34 @@ const SEO = () => (
       <meta lang="pt-br" />
       <meta property="og:type" content="website" />
       <meta property="og:title" content="Karleon Cristophe" />
-      <meta property="og:url" content="https://www.karleoncristophe.com.br/" />
+      <meta property="og:url" content="https://karleoncristophe.com.br/" />
       <link rel="icon" href="/favicon.ico" />
     </Head>
     <NextSeo
       title="Karleon Cristophe"
       description="Programador web/mobile freelancer, criação de sites, aplicativos móveis, landing pages, front e backend."
-      canonical="https://www.karleoncristophe.com.br/"
+      canonical="https://karleoncristophe.com.br/"
       openGraph={{
-        url: 'https://www.karleoncristophe.com.br/',
+        url: 'https://karleoncristophe.com.br/',
         title: 'Karleon Cristophe',
 
         description:
           'Programador web/mobile freelancer, criação de sites, aplicativos móveis, landing pages, front e backend.',
         images: [
           {
-            url: 'https://www.karleoncristophe.com.br/programming.svg',
+            url: 'https://karleoncristophe.com.br/vercel.svg',
             width: 900,
             height: 600,
             alt: 'karleoncristophe',
           },
           {
-            url: 'https://www.karleoncristophe.com.br/programming.svg',
+            url: 'https://karleoncristophe.com.br/vercel.svg',
             width: 900,
             height: 800,
             alt: 'karleoncristophe',
           },
-          { url: 'https://www.karleoncristophe.com.br/programming.svg' },
-          { url: 'https://www.karleoncristophe.com.br/programming.svg' },
+          { url: 'https://karleoncristophe.com.br/vercel.svg' },
+          { url: 'https://karleoncristophe.com.br/vercel.svg' },
         ],
         site_name: 'SiteName',
       }}
